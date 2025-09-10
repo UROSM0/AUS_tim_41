55ce50aebb011c53f1e749d5b762434143d732ef
AndrejaJ13 <andreja.jovanic13@gmail.com>
2025-09-10 18:20:58 +0200

    analogno pisanje

--- Modbus/ModbusFunctions/WriteSingleCoilFunction.cs
+++ Modbus/ModbusFunctions/WriteSingleCoilFunction.cs
@@ -24,15 +24,32 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc />
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusWriteCommandParameters parametri = this.CommandParameters as ModbusWriteCommandParameters;
+            byte[] paket = new byte[12];
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)parametri.TransactionId)), 0, paket, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)parametri.ProtocolId)), 0, paket, 2, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)parametri.Length)), 0, paket, 4, 2);
+            paket[6] = parametri.UnitId;
+            paket[7] = parametri.FunctionCode;
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)parametri.OutputAddress)), 0, paket, 8, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)parametri.Value)), 0, paket, 10, 2);
+            return paket;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            var dict = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            var adresa = BitConverter.ToUInt16(response, 8);
+            var vrijednost = BitConverter.ToUInt16(response, 10);
+
+            adresa = (ushort)IPAddress.NetworkToHostOrder((short)adresa);
+            vrijednost = (ushort)IPAddress.NetworkToHostOrder((short)vrijednost);
+
+            dict.Add(new Tuple<PointType, ushort>(PointType.DIGITAL_OUTPUT, adresa), vrijednost);
+
+            return dict;
         }
     }
 }
