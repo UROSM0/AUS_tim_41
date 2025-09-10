59801fd4b0148d50e1580cab51da6d21c834b744
UROSM0 <uros.milosevic03@gmail.com>
2025-09-10 18:08:19 +0200

    readCoilsFunction

--- Modbus/ModbusFunctions/ReadCoilsFunction.cs
+++ Modbus/ModbusFunctions/ReadCoilsFunction.cs
@@ -24,15 +24,43 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc/>
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusReadCommandParameters parametri = this.CommandParameters as ModbusReadCommandParameters;
+            byte[] paketi = new byte[12];
+            Buffer.BlockCopy((Array)BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)parametri.TransactionId)), 0, paketi, 0, 2);
+            Buffer.BlockCopy((Array)BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)parametri.ProtocolId)), 0, paketi, 2, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)parametri.Length)), 0, paketi, 4, 2);
+            paketi[6] = parametri.UnitId;
+            paketi[7] = parametri.FunctionCode;
+            Buffer.BlockCopy((Array)BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)parametri.StartAddress)), 0, paketi, 8, 2);
+            Buffer.BlockCopy((Array)BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)parametri.Quantity)), 0, paketi, 10, 2);
+            return paketi;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusReadCommandParameters mpar = this.CommandParameters as ModbusReadCommandParameters;
+            Dictionary<Tuple<PointType, ushort>, ushort> dict = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            ushort quantity = response[8]; //koliko bajtova treba citati; ByteCount - br BAJTOVA
+
+            ushort value;
+            Console.WriteLine(quantity);
+            for (int i = 0; i < quantity; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    value = (ushort)(response[9 + i] & (byte)0x1);  //logicko i sa maskom cija je vrijednost 1
+                    response[9 + i] /= 2;   //siftovanje
+
+                    if (mpar.Quantity < (j + i * 8)) { break; } //Quantity je br BITOVA
+
+
+                    dict.Add(new Tuple<PointType, ushort>(PointType.DIGITAL_OUTPUT, (ushort)(mpar.StartAddress + (j + i * 8))), value);
+                }
+
+            }
+            return dict;
         }
     }
 }
